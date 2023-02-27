e4d1812c4e1bf994d9ada83b7e5addeff789934f
Cristover Wurangian <christwurangian@gmail.com>
2023-02-27 16:22:01 +0700

    initial commit

---
+++ WomsAuth/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WomsAuth;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ WomsAuth/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WomsAuth;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
